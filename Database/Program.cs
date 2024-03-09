d5708c90d812b759b86c63cb1edd8e55bc0c8a37
vaibhav <vaibhavnagargoje2000@gmail.com>
2024-03-09 14:08:07 +0530

    first commit

---
+++ Database/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (FileStream fs = File.OpenWrite("../../../Employee.txt")) 
+                {
+                string text = "employee data \n Id : 1\n name : Vaibhav \n city: pune \n salary : 234242\n";
+                byte[] data = Encoding.UTF8.GetBytes(text);
+                fs.Write(data, 0, data.Length);
+                }
+                //using (StreamWriter sw )
+
+
+
+            using (StreamWriter sw = File.AppendText("../../../Employee.txt"))
+            {
+
+                string text1 = "name: santosh\n city: karmala \n salary : 5000";
+                sw.WriteLine(text1);
+
+            }
+
+
+
+
+
+            using(FileStream fs = File.OpenRead("../../../Employee.txt"))
+                {
+                    byte[] b = new byte[fs.Length];
+                    fs.Read(b, 0, b.Length);
+                    Console.WriteLine(Encoding.UTF8.GetString(b));
+                }
+                if (File.Exists("../../../Employee.txt"))
+                {
+                    //
+                }
+
+
+
+            }
+            catch (Exception ex){
+                Console.WriteLine(ex.Message);
+                
+            }
+
+
+            //#region  FileInfo
+            DirectoryInfo directoryInfo = new DirectoryInfo("../../demoFolder");
+            Console.WriteLine("Current direactory name: {0} ",directoryInfo.FullName);
+            Console.WriteLine("Current direactory name: {0} ",directoryInfo.CreationTime);
+            Console.WriteLine("chiled directories :\n");
+
+            foreach(DirectoryInfo dir in directoryInfo.GetDirectories())
+            {
+                Console.WriteLine("Directtory Name :{0}", dir.FullName);
+                Console.WriteLine("Directtory Name :{0}", dir.CreationTime);
+                Console.WriteLine("Directtory Name :{0}", dir.Extension);
+                Console.WriteLine("--------------------------------------------");
+            }
+
+
+
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ XML_Project/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Xml;
+
+namespace XML_Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                
+
+            Console.WriteLine("");
+             XmlDocument xmlDoc = new XmlDocument();
+                //xmlDoc.Load("C:\\Users\\91976\\Desktop\\Full Stack\\dotNet\\hand on\\Employee.xml");
+                xmlDoc.Load("..\\..\\..\\Employee.xml");
+                xmlDoc.SelectNodes("/Employees/Employee");
+                var employeeNodes = xmlDoc.SelectNodes("/Employees/Employee");
+
+
+                foreach (XmlNode node in employeeNodes)
+                {
+                    Console.WriteLine("Name: {0} City: {1}, {2} ", node["name"].InnerText, node["City"].InnerText, node["Salary"].InnerText );
+
+                }
+                var OneEmp = xmlDoc.SelectSingleNode("/Employees/Employee[@id=1]");
+                Console.WriteLine("Name: {0} City: {1}, Salary : {2} ", OneEmp["name"].InnerText, OneEmp["City"].InnerText, OneEmp["Salary"].InnerText);
+
+                // add new node 
+                var rootNode = xmlDoc.SelectSingleNode("/Employees");
+                var newEmployee = xmlDoc.CreateElement("Employee", null);
+                newEmployee.SetAttribute("id", "5");
+
+                XmlNode nameNode = xmlDoc.CreateNode(XmlNodeType.Element, "name", null);
+                nameNode.InnerText = "Raj";
+
+
+
+                XmlNode CityName = xmlDoc.CreateNode(XmlNodeType.Element, "City", null);
+                CityName.InnerText = "Mumbai";
+
+                XmlNode SalaryNode = xmlDoc.CreateNode(XmlNodeType.Element, "Salary",null);
+                SalaryNode.InnerText = "1231231231";
+
+                newEmployee.AppendChild(nameNode);
+                newEmployee.AppendChild(CityName);
+                newEmployee.AppendChild(SalaryNode);
+
+                xmlDoc.DocumentElement.AppendChild(newEmployee);
+                xmlDoc.Save("C:\\Users\\91976\\Desktop\\Full Stack\\dotNet\\hand on\\Employee.xml");
+
+                foreach (XmlNode node in employeeNodes)
+                {
+                    Console.WriteLine("Name: {0} City: {1}, {2} ", node["name"].InnerText, node["City"].InnerText, node["Salary"].InnerText);
+
+                }
+            }
+
+            catch { 
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
